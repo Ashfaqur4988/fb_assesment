f1e6ee5c41c00cedde9e7cb1a52097be96d87f34
ZiZi07 <ashfakurriaz@gmail.com>
2024-12-01 11:56:10 +0530

    signup path corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import WorkFlowPage from "./pages/WorkFlowPage";
 import Navbar from "./components/navbar/Navbar";
-import Signup from "./pages/signup";
 import Login from "./pages/Login";
 import { useAuthStore } from "./store/useAuthStore";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
+import Signup from "./pages/signup";
 
 function App() {
   const { user, checkingAuth, checkAuth } = useAuthStore();
